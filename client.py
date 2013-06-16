ed3f69193b0c2df111fd810a2241c8667a1111a3
Josh Taylor <joshotaylor@gmail.com>
2013-06-16 20:46:41 +0100

    Hostname and port number can now be specified when starting the client

--- client.py
+++ client.py
@@ -4,18 +4,17 @@ import sys, getopt
 from lib import notifications
 import socket
 
-host = socket.gethostname()
-port = 1337
-
 def main(argv):
 	command = ''
 	arguments = []
 
 	try:
-		opts, args = getopt.getopt(argv, "hc:a:")
+		opts, args = getopt.getopt(argv, "hc:a:n:p:")
 	except getopt.GetOptError:
 		help()
 
+	host = None
+	port = None
 	for opt, arg in opts:
 		if opt == "-h":
 			help()
@@ -23,6 +22,15 @@ def main(argv):
 			command = arg
 		elif opt == "-a":
 			arguments.append(arg);
+		elif opt == "-n":
+			host = arg
+		elif opt == "-p":
+			port = arg
+
+	if host == None:
+		host = socket.gethostname()
+	if port == None:
+		port = 1337
 
 	s = socket.socket()
 	s.connect((host, port))
