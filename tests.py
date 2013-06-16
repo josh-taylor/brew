86fae3bac34a11dcadc3358c548e70f1e9bd783b
Josh Taylor <joshotaylor@gmail.com>
2013-06-16 21:16:16 +0100

    Refactored out the unit tests to make it easier to add to in the future

--- tests.py
+++ tests.py
@@ -1,28 +1,5 @@
 import unittest
-from server import Tally, User
 
-class TestTally(unittest.TestCase):
-
-	def setUp(self):
-		self.tally = Tally([User('foo'), User('bar')])
-
-	def test_all_voted(self):
-		self.tally.set_voted('foo', True)
-		self.tally.set_voted('bar', True)
-
-		self.assertTrue(self.tally.all_voted())
-
-	def test_all_voted_false(self):
-		self.tally.set_voted('foo', True)
-		self.tally.set_voted('bar', False)
-
-		self.assertFalse(self.tally.all_voted())
-
-	def test_get_brews(self):
-		self.tally.add_to('foo')
-		self.tally.add_to('foo')
-
-		self.assertEquals(2, self.tally.get_brews('foo'))
-
-if __name__ == '__main__':
-	unittest.main()
+tests = unittest.defaultTestLoader.discover('tests')
+testRunner = unittest.TextTestRunner()
+testRunner.run(tests)
